520763494f744155dd3c35b3795887e1c191b8e4
MehrnazMirahmadi <125347723+MehrnazMirahmadi@users.noreply.github.com>
2025-05-13 16:38:26 +0330

    Completed UI

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -2,5 +2,7 @@
 global using Shopping.Web.Services;
 global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Ordering;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
 global using Refit;
 global using System.Net;
